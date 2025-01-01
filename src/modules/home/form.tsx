93a079dfd7e06596b688ebefc2c1c28a18d80328
henzyd <henzydee@gmail.com>
2025-01-01 23:44:12 +0100

    updated form

--- src/modules/home/form.tsx
+++ src/modules/home/form.tsx
@@ -2,6 +2,7 @@ import { Formik } from "formik";
 import * as Yup from "yup";
 import FormField from "~/components/fields/form-field";
 import { Button } from "~/components/ui/button";
+import FileField from "~/components/fields/file-field";
 
 const validationSchema = Yup.object({
   title: Yup.string().required("Title is required"),
@@ -21,22 +22,19 @@ const initialValues: Record<"title" | "subtitle" | "description", string> & {
 export default function Form() {
   return (
     <Formik
-      client:load
       initialValues={initialValues}
       validationSchema={validationSchema}
       onSubmit={async () => {}}
+      validateOnBlur={false}
+      validateOnChange={false}
     >
       {({ handleSubmit, isSubmitting }) => (
-        <form
-          onSubmit={handleSubmit}
-          method="post"
-          className="flex flex-col gap-4"
-        >
+        <form onSubmit={handleSubmit} method="POST" className="flex flex-col gap-4">
+          <FileField name="image" label={"Image"} required preview />
           <FormField name="title" label="Title" />
           <FormField name="subtitle" label="Subtitle" />
-          <FormField name="subtitle" label="Description" />
-
-          <Button isLoading={isSubmitting} type="submit" className="self-end">
+          <FormField name="description" label="Description" />
+          <Button isLoading={isSubmitting} type="submit" className="mt-2 self-end">
             Submit
           </Button>
         </form>
