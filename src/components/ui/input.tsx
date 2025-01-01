fac262de2e564239895233a6044dcb40d90349c1
henzyd <henzydee@gmail.com>
2025-01-01 23:41:09 +0100

    updated components ui

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -12,14 +12,12 @@ const Input = React.forwardRef<HTMLInputElement, Props>(
     return (
       <div className="relative flex items-center">
         {startAdornment && (
-          <span className="absolute left-3 flex items-center">
-            {startAdornment}
-          </span>
+          <span className="absolute left-3 flex items-center">{startAdornment}</span>
         )}
         <input
           type={type}
           className={cn(
-            "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+            "flex h-10 w-full rounded-md border-none border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
             startAdornment ? "pl-10" : "",
             endAdornment ? "pr-10" : "",
             className
@@ -27,11 +25,7 @@ const Input = React.forwardRef<HTMLInputElement, Props>(
           ref={ref}
           {...props}
         />
-        {endAdornment && (
-          <span className="absolute right-3 flex items-center">
-            {endAdornment}
-          </span>
-        )}
+        {endAdornment && <span className="absolute right-3 flex items-center">{endAdornment}</span>}
       </div>
     );
   }
